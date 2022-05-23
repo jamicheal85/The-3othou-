af63d8e7509333069cf21ab8861e5c3ed0a54b3e
jamicheal <jamicheal.ceo@gmail.com>
2022-05-23 18:28:20 -0500

    inital commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Ceo of 30 Thou!");
+        }
+    }
+}
